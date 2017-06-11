586541328f6e7aca984597cc04b8c4d7e2bdb50b
Nathan Ng <n.ng555@gmail.com>
2017-06-10 17:53:45 -0700

    Added customers page

--- Model/Appointment.js
+++ Model/Appointment.js
@@ -1,13 +1,18 @@
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://test:test@ds141401.mlab.com:41401/shinypigeons_test');
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open',function(){
     // we're connected
     console.log("we are connected");
 });
+
 var appointmentSchema = mongoose.Schema({
-    EmployeeID: Number,
+    firstName: String,
+    lastName: String,
+    phone: String,
     timeOf: Date,
     reasonForAppointment: String,
     methodOfCommunication: String,
@@ -16,19 +21,21 @@ var appointmentSchema = mongoose.Schema({
     createdOn: { type: Date, default: Date.now }
 });
 
-var appointmentModel = mongoose.model('Appointments', appointmentSchema);
+var appointmentModel = mongoose.model('appointments', appointmentSchema);
+
 module.exports = function(req,res) {
     var doc = new appointmentModel({
         timeOf: Date.now(),
         reasonForAppointment: "Business",
         methodOfCommunication: "SMS",
         ratingReceived: 5,
-        EmployeeID: 2
+        firstName: "Beter",
+        lastName: "Griffin"
     });
     doc.save(function (err) {
         if (err) {
-            console.log(err) 
-            return   
+            console.log(err)
+            return
     }
     else {
         // Saved
