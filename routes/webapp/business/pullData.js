3971c9da5c39116a5de12d131a47a1cd4dfcc18b
IceHism <kleung087@gmail.com>
2017-06-12 16:36:08 -0700

    got JS objects from database

--- routes/webapp/business/pullData.js
+++ routes/webapp/business/pullData.js
@@ -4,36 +4,43 @@ var appointments = require('../../../model/appointments.js');
 var businesses = require('../../../model/businesses.js');
 var customers = require('../../../model/customers.js');
 
+
 exports.get = function(req, res) {
   var db = req.db;
-  appointments.find(function (err, appointments) {
+  appointments.find().populate('customer').lean().exec(function (err, appointments) {
     if (err) {
         // Note that this error doesn't mean nothing was found,
         // it means the database had an error while searching, hence the 500 status
         res.status(500).send(err)
     } else {
+        var appointmentsData = appointments;
+        console.log(appointmentsData);
         // send the list of all people
-        res.send(appointments);
+        //res.send(appointments);
     }
   });
-  businesses.find(function (err, businesses) {
+  businesses.find().lean().exec(function (err, businesses) {
     if (err) {
         // Note that this error doesn't mean nothing was found,
         // it means the database had an error while searching, hence the 500 status
         res.status(500).send(err)
     } else {
+        var businessesData = businesses;
+        console.log(businessesData);
         // send the list of all business
-        res.send(businesses);
+        //res.send(businesses);
     }
   });
-  customers.find(function (err, customers) {
+  customers.find().populate('business').lean().exec(function (err, customers) {
     if (err) {
         // Note that this error doesn't mean nothing was found,
         // it means the database had an error while searching, hence the 500 status
         res.status(500).send(err)
     } else {
+        var customersData = customers;
+        console.log(customersData);
         // send the list of all customers
-        res.send(customers);
+        //res.send(customers);
     }
   });
 }
