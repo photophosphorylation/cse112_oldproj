ae5ff3dad3b067770cd2d0979a7750b12e3e2f2d
Paris Do <pcdo@ucsd.edu>
2017-06-13 19:11:36 -0700

    changed landing page and sign in/sign up

---
+++ public/javascripts/scrolling-nav.js
@@ -0,0 +1,20 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".container-fluid ").css("background", "white");
+        $(".navbar-default .navbar-nav > li > a").css("color", "#8a959e");
+        $("a.joinNow ").css("background", "#ffae00");
+        $("a.joinNow ").css("color", "white !important");
+        $("a.joinNow ").css("border", "none");
+
+        // $("#navbar-back").css("background", "rgba(34, 53, 71, 1)");
+    } else {
+        $(".container-fluid ").css("transition", "background 0.5s ease");
+        $(".container-fluid ").css("background", "transparent");
+        $(".navbar-default .navbar-nav > li > a").css("color", "white");
+        $("a.joinNow").css("color", "#ffae00 !improtant");
+        $("a.joinNow ").css("background", "transparent");
+        $("a.joinNow ").css("color", "#ffae00 !important");
+        $("a.joinNow ").css("border", "1px solid #ffae00");
+    }
+});
