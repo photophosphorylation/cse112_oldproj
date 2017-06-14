291bc331b04f21fb71b63ad6e648f71c7d5341ff
IceHism <kleung087@gmail.com>
2017-06-14 01:22:36 -0700

    getting data from appointments kind of working

--- routes/webapp/business/pullData.js
+++ routes/webapp/business/pullData.js
@@ -4,11 +4,12 @@ var appointments = require('../../../model/appointments.js');
 
 exports.get = function(req, res) {
   var db = req.db;
-  appointments.find().populate('customer business').exec(function (err, appointments) {
+  console.log(req.user[0].business);
+  appointments.find(/*{'business._id': req.user[0].business}*/).populate('customer business').exec(function (err, appointments) {
     if (err) {
         // Note that this error doesn't mean nothing was found,
         // it means the database had an error while searching, hence the 500 status
-        res.status(500).send(err)
+        res.status(500).send(err);
     } else {
         // send the list of all people
         res.send(appointments);
